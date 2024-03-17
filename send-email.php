baaabfc91d26faa87c23114725d4a7165700b0dc
VKrieger <krieger.vi@gmail.com>
2024-03-18 00:27:50 +0100

    working phpmailer

--- send-email.php
+++ send-email.php
@@ -6,30 +6,41 @@ $betreff = $_POST["betreff"];
 $nachricht = $_POST["nachricht"];
 $Datenverarbeitung = $_POST["Datenverarbeitung-Einwilligung"];
 
-require "./vendor/autoload.php";
+require_once "./vendor/autoload.php";
+
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
+
 $mail = new PHPMailer(true);
+
+$mail->CharSet = "UTF-8";
+
 
 $mail->isSMTP();
 $mail->SMTPAuth = true;
 
-$mail->HOST = "	smtp.ionos.de";
-$mial->SMTPSecure = "PHPMailer::ENCRYPTION_STARTTLS";
+$mail->Host = "smtp.ionos.de";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->Port = 587;
 
 
 
-$mail->setForm($email, $name);
-$mail->addAddress("kontaktformular@tg-jahn-namedy.de");
 
+$mail->setFrom("kontaktformular@tg-jahn-namedy.de", "Webseite Kontakt");
+$mail->addAddress("kontaktformular@tg-jahn-namedy.de", "Webseite Kontakt");
+$mail->addReplyTo($email, $name);
+
+$mail->isHTML(true);
 $mail->Subject = $betreff;
-$mail->Body = $nachricht;
+
+$inhalt = "Name: " . $name . "<br/>Betreff: " . $betreff . "<br/>E-Mail: " . $email . "<br/>Nachricht: " . $nachricht . "";
+$mail->Body = $inhalt;
 
 $mail->send();
 
-header("Location: sent.html");
 
-?>
+
+header("Location: ./form_send.html#form_send");
+
